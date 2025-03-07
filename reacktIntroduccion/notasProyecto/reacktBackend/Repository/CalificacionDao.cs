db3c1a030680777a3908906f739833d170d257b4
Rene Lemus <lm0298032023@unab.edu.sv>
2025-03-07 15:40:23 -0600

    Guia Vl - Realizada

---
+++ reacktIntroduccion/notasProyecto/reacktBackend/Repository/CalificacionDao.cs
@@ -0,0 +1,81 @@
+﻿using reactBackend.Context;
+using Microsoft.Identity.Client;
+using reactBackend.Context;
+using reactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Repository
+{
+    public class CalificacionDao
+    {
+        private RegistroAlumnosContext _contexto = new RegistroAlumnosContext();
+
+        public List<Calificacion> seleccion(int matriculaId)
+        {
+            var matricula = _contexto.Matriculas.Where(x => x.Id == matriculaId);
+            Console.WriteLine("Matricula encontrada");
+
+            try
+            {
+                if (matricula != null)
+                {
+                    var calificacion = _contexto.Calificacions.Where(x => x.Id == matriculaId).ToList();
+                    return calificacion;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public bool insertar(Calificacion calificacion)
+        {
+            try
+            {
+                if (calificacion == null)
+                {
+                    return false;
+                }
+
+                var addCalificacion = _contexto.Calificacions.Add(calificacion);
+                _contexto.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool eliminarCalificacion(int id)
+        {
+            var calificacion = _contexto.Calificacions.Where(x => x.Id == id).FirstOrDefault();
+
+            try
+            {
+                if (calificacion == null)
+                {
+                    return false;
+                }
+
+                var addCalificacion = _contexto.Calificacions.Remove(calificacion);
+                _contexto.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
