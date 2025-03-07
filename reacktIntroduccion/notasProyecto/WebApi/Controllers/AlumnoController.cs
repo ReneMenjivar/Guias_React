41bd28334eb578cc0310016c5240218af7227a58
Rene Lemus <lm0298032023@unab.edu.sv>
2025-03-06 22:20:25 -0600

    Guia 8 - LIstado de alumnos de un profesor

---
+++ reacktIntroduccion/notasProyecto/WebApi/Controllers/AlumnoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+        private AlumnoDao _dao = new AlumnoDao();
+
+        [HttpGet("AlumnoProfesors")]
+        public List<AlumnoProfesor> GetAlumnoProfesor(string usuario)
+        {
+            return _dao.AlumnoProfesors(usuario);
+        }
+    }
+}
--- reacktIntroduccion/notasProyecto/reacktBackend/Repository/AlumnoDAO.cs
+++ reacktIntroduccion/notasProyecto/reacktBackend/Repository/AlumnoDAO.cs
@@ -128,25 +128,25 @@ namespace reactBackend.Repository
 
         }
 
-        //public List<AlumnoProfesor> AlumnoProfesors(string nombreProfesor)
-        //{
-        //    var listadoALumno = from a in contexto.Alumnos
-        //                        join m in contexto.Matriculas on a.Id equals m.AlumnoId
-        //                        join asig in contexto.Asignaturas on m.AsignaturaId equals asig.Id
-        //                        where asig.Profesor == nombreProfesor
-        //                        select new AlumnoProfesor
-        //                        {
-        //                            Id = a.Id,
-        //                            Dni = a.Dni,
-        //                            Nombre = a.Nombre,
-        //                            Direccion = a.Direccion,
-        //                            Edad = a.Edad,
-        //                            Email = a.Email,
-        //                            Asignatura = asig.Nombre
-        //                        };
+        public List<AlumnoProfesor> AlumnoProfesors(string nombreProfesor)
+        {
+            var listadoALumno = from a in contexto.Alumnos
+                                join m in contexto.Matriculas on a.Id equals m.AlumnoId
+                                join asig in contexto.Asignaturas on m.AsignaturaId equals asig.Id
+                                where asig.Profesor == nombreProfesor
+                                select new AlumnoProfesor
+                                {
+                                    Id = a.Id,
+                                    Dni = a.Dni,
+                                    Nombre = a.Nombre,
+                                    Direccion = a.Direccion,
+                                    Edad = a.Edad,
+                                    Email = a.Email,
+                                    Asignatura = asig.Nombre
+                                };
 
-        //    return listadoALumno.ToList();
-        //}
+            return listadoALumno.ToList();
+        }
 
         public Alumno DNIAlumno(Alumno alumno)
         {
